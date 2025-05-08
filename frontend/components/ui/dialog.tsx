f5eb68d5570351d71f3b0010e2dec851d4c5b01a
Mouhamadou Sarr <mouhamadoul.sarr@gmail.com>
2025-05-08 22:40:44 +0200

    chore: Reorganize imports in button and dialog components for improved consistency and readability

--- frontend/components/ui/dialog.tsx
+++ frontend/components/ui/dialog.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -97,13 +97,13 @@ DialogDescription.displayName = DialogPrimitive.Description.displayName;
 
 export {
   Dialog,
-  DialogPortal,
-  DialogOverlay,
   DialogClose,
-  DialogTrigger,
   DialogContent,
-  DialogHeader,
-  DialogFooter,
-  DialogTitle,
   DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
 };
