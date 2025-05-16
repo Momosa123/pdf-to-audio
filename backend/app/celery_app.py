03ca60a70957ce3078cf466bf991cdda3283ac1d
Mouhamadou Sarr <mouhamadoul.sarr@gmail.com>
2025-05-16 14:58:18 +0200

    chore: Update Celery configuration to use environment variable for Redis URL

--- backend/app/celery_app.py
+++ backend/app/celery_app.py
@@ -1,8 +1,13 @@
+import os
+
 from celery import Celery
+
+# Utiliser la variable d'environnement REDIS_URL, avec une valeur par défaut
+redis_url = os.getenv("REDIS_URL", "redis://redis:6379/0")
 
 celery_app = Celery(
     "pdf_to_audio",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0",
+    broker=redis_url,
+    backend=redis_url,
     include=["app.services.tts_task"],
 )
